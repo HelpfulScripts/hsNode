15bbec48dd25dd19c6851ea6f585fedc5f03e339
hs <>
2018-07-12 12:11:35 -0700

    refactored log to a class

--- src/log.ts
+++ src/log.ts
@@ -38,22 +38,29 @@
  * - [ERROR](#error)
  * 
  * ## Reporting methods
- * - {@link hsNode.log#methods_debug debug()}
- * - {@link hsNode.log#methods_info info()}
- * - {@link hsNode.log#methods_warn warn()}
- * - {@link hsNode.log#methods_error error()}
+ * - &nbsp;{@link hsNode.log#methods_debug debug()}
+ * - &nbsp;{@link hsNode.log#methods_info info()}
+ * - &nbsp;{@link hsNode.log#methods_warn warn()}
+ * - &nbsp;{@link hsNode.log#methods_error error()}
  * 
  * ## Configurations:
- * - {@link hsNode.log#methods_level level()}
- * - {@link hsNode.log#methods_format format()}
- * - {@link hsNode.log#methods_prefix prefix()}
- * - {@link hsNode.log#methods_logFile logFile()}
+ * - &nbsp;{@link hsNode.log#methods_level level()}
+ * - &nbsp;{@link hsNode.log#methods_format format()}
+ * - &nbsp;{@link hsNode.log#methods_prefix prefix()}
+ * - &nbsp;{@link hsNode.log#methods_logFile logFile()}
  */
 
 /** importing nodejs file system function; needed to create logfiles */
-import { hsNode } from "./";
-import { fsUtil } from "./";
-import { date } from 'hsutil';
+import { inspect }  from 'util';
+import { fsUtil }   from "./";
+import { date }     from 'hsutil';
+
+
+
+/**
+ * Type definition for level descriptors
+ */
+interface LevelDesc { importance:number; sym:symbol; desc:string; };
 
 /** Debug reporting level with importance 0 */
 const DEBUG         = Symbol('DEBUG');
@@ -67,10 +74,6 @@ const WARN          = Symbol('WARN');
 /** Warning reporting level with importance 3 */
 const ERROR         = Symbol('ERROR');
 
-/**
- * Type definition for level descriptors
- */
-interface LevelDesc { importance:number; sym:symbol; desc:string; };
 
 /** map of valid reporting levels */
 const gLevels = {
@@ -81,7 +84,7 @@ const gLevels = {
 };
 
 /** current reporting level, same across all modules */
-var gLevel:LevelDesc = (gLevel===undefined)? gLevels[INFO] : gLevel; 
+let gLevel:LevelDesc = gLevels[INFO]; 
 console.log('set log level to ' + gLevel.sym.toString());
 
 /** current date format string. See [date module]('_date_.html') */
@@ -90,160 +93,150 @@ let gDateFormat   = '%YYYY%MM%DD %hh:%mm:%ss.%jjj';
 /** name of the current log file, or undefined */
 let gLogFile: string;	// initially disabled
 
+/** boolean determining if log will be printed in color */
+let gColors = true;
 
-function log() {
-	let gPrefix = '';
-    let gColors = true;
 
-	/**
+class Log {
+    private gPrefix = '';
+    DEBUG   = DEBUG;
+    INFO    = INFO;
+    WARN    = WARN;
+    ERROR   = ERROR;
+
+    /**
      * sets the reporting level according to `newLevel`. 
-	 * Valid values are {@link hsNode.log.DEBUG DEBUG}, {@link hsNode.log.INFO INFO}, {@link hsNode.log.WARN WARN}, or {@link hsNode.log.ERROR ERROR}.
-	 * Subsequent reporting calls
-	 * will be filtered such that only calls with an importance at least the same as 
-	 * `newLevel` will be written to the log.
-	 * @param {String=} newLevel the new reporting level to set. 
-	 * If omitted, the method returns the currently set reporting level. 
-	 * @return {Symbol} the new reporting level (DEBUG, INFO, ERROR)
-	 */
-	function level(newLevel?:symbol):symbol {
-	    if (newLevel) { 
-	        if (gLevels[newLevel]) { 
-	        	let oldLevel = gLevel;
-	            gLevel = gLevels[newLevel];
-	            let msg = 'new log level \'' + gLevel.desc.toUpperCase() + '\' (was ' + oldLevel.desc.toUpperCase() + ')';
-	            out((gLevel.sym === oldLevel.sym)?DEBUG : INFO, msg);
-	        }
-	        else { out(ERROR, "unkown level " + newLevel.toString() + '; log level remains ' + gLevel.sym.toString()); }
-	    }
-	    return gLevel.sym;
-	}
+     * Valid values are {@link hsNode.log.DEBUG DEBUG}, {@link hsNode.log.INFO INFO}, {@link hsNode.log.WARN WARN}, or {@link hsNode.log.ERROR ERROR}.
+     * Subsequent reporting calls
+     * will be filtered such that only calls with an importance at least the same as 
+     * `newLevel` will be written to the log.
+     * @param {String=} newLevel the new reporting level to set. 
+     * If omitted, the method returns the currently set reporting level. 
+     * @return {Symbol} the new reporting level (DEBUG, INFO, ERROR)
+     */
+    level(newLevel?:symbol):symbol {
+        if (newLevel) { 
+            if (gLevels[newLevel]) { 
+                let oldLevel = gLevel;
+                gLevel = gLevels[newLevel];
+                let msg = 'new log level \'' + gLevel.desc.toUpperCase() + '\' (was ' + oldLevel.desc.toUpperCase() + ')';
+                this.out((gLevel.sym === oldLevel.sym)?DEBUG : INFO, msg);
+            }
+            else { this.out(ERROR, "unkown level " + newLevel.toString() + '; log level remains ' + gLevel.sym.toString()); }
+        }
+        return gLevel.sym;
+    }
 
-	/**
+    /**
      * reports an debug message to the log. 
-	 * The message will actually be reported to the log only if the current 
-	 * reporting level is DEBUG or lower.
-	 * @param {string} msg the message to report.
-	 */
-	function debug(msg:string) { out(DEBUG, msg); }
+     * The message will actually be reported to the log only if the current 
+     * reporting level is DEBUG or lower.
+     * @param {string} msg the message to report.
+     */
+    debug(msg:string) { this.out(DEBUG, msg); }
 
-	/**
+    /**
      * reports an informational message to the log. 
-	 * The message will actually be reported to the log only if the current 
-	 * reporting level is INFO or lower.
-	 * @param {string} msg the message to report.
-	 */
-	function info(msg:string)  { out(INFO, msg); }
+     * The message will actually be reported to the log only if the current 
+     * reporting level is INFO or lower.
+     * @param {string} msg the message to report.
+     */
+    info(msg:string)  { this.out(INFO, msg); }
 
-	/**
+    /**
      * reports an warning message to the log. 
-	 * The message will actually be reported to the log only if the current 
-	 * reporting level is WARN or lower.
-	 * @param {string} msg the message to report.
-	 */
-	function warn(msg:string) { out(WARN, msg); }
+     * The message will actually be reported to the log only if the current 
+     * reporting level is WARN or lower.
+     * @param {string} msg the message to report.
+     */
+    warn(msg:string) { this.out(WARN, msg); }
 
-	/**
+    /**
      * reports an error message to the log. 
-	 * The message will always be reported to the log.
-	 * @param {string} msg the message to report.
-	 */
-	function error(msg:string) { out(ERROR, msg); }
+     * The message will always be reported to the log.
+     * @param {string} msg the message to report.
+     */
+    error(msg:string) { this.out(ERROR, msg); }
 
-	/**
+    /**
      * sets the format string to use for logging. If no parameter is specified,
-	 * the function returns the currently set format string. The preset is '%YYYY%MM%DD %hh:%mm:%ss.%jjj'
+     * the function returns the currently set format string. The preset is '%YYYY%MM%DD %hh:%mm:%ss.%jjj'
      * For supported formats see {@link date date}.
-	 * @param {String=} fmtStr the format string to use. 
-	 * @return {String} the currently set format string
-	 */
-	function dateFormat(fmtStr?:string):string { 
-	    if (fmtStr) { gDateFormat = fmtStr; }
-	    return gDateFormat;
-	}
+     * @param {String=} fmtStr the format string to use. 
+     * @return {String} the currently set format string
+     */
+    dateFormat(fmtStr?:string):string { 
+        if (fmtStr) { gDateFormat = fmtStr; }
+        return gDateFormat;
+    }
 
-	/**
+    /**
      * defines a prefix to be printed for each call to a log function. 
-	 * The return object contains all functions defined for export. 
-	 * @param {String=} prf the prefix to prepend. 
-	 */
-	function prefix(prf=''):void {
-        gPrefix = prf? prf + ' ' : '';
-	}
+     * The return object contains all functions defined for export. 
+     * @param {String=} prf the prefix to prepend. 
+     */
+    prefix(prf=''):void {
+        this.gPrefix = prf? prf + ' ' : '';
+    }
 
-	/**
+    /**
      * sets a new logfile name template. Logfiles are created using this template 
-	 * at the time of each log entry call. If the file exists, the log entry will be appended.
-	 * @param {String} [fileNameTemplate='log-%YYYY-%MM-%DD.txt'] a template to use for log file names. 
-	 * To disable logging, use file=''.
-	 * @return {Promise} promise to return the current logfile name template
-	 */
-	function logFile(file='log-%YYYY-%MM-%DD.txt'):Promise<string> {
+     * at the time of each log entry call. If the file exists, the log entry will be appended.
+     * @param {String} [fileNameTemplate='log-%YYYY-%MM-%DD.txt'] a template to use for log file names. 
+     * To disable logging, use file=''.
+     * @return {Promise} promise to return the current logfile name template
+     */
+    logFile(file='log-%YYYY-%MM-%DD.txt'):Promise<string> {
         return Promise.resolve(file)
             .then(file => {
                 if (file !== gLogFile) {
                     gLogFile = (file==='')? undefined : file;
                 }
-                if (!gLogFile) { info("disabling logfile"); return gLogFile; }
+                if (!gLogFile) { this.info("disabling logfile"); return gLogFile; }
                 if (gLogFile.indexOf('/')>=0) { 
                     const dir = gLogFile.substring(0, gLogFile.lastIndexOf('/'));
                     return fsUtil.pathExists(dir).then(exists => {
                         if (!exists) { 
                             gLogFile = undefined; 
-                            warn(`path ${dir} doesn't exists; logfile disabled`); 
+                            this.warn(`path ${dir} doesn't exists; logfile disabled`); 
                         }
-                        else { info(gLogFile? "now logging to file " + date(gLogFile) : "disabling logfile"); }
+                        else { this.info(gLogFile? "now logging to file " + date(gLogFile) : "disabling logfile"); }
                         return gLogFile;
                     });
                 }
-                info("now logging to file " + date(gLogFile));
+                this.info("now logging to file " + date(gLogFile));
                 return gLogFile;
             });
-	}
-	
-	function out(sym:symbol, msg:any) {	
+    }
+
+    out(sym:symbol, msg:any) {	
         const color = { ERROR: '\x1b[31m\x1b[1m', WARN: '\x1b[33m', DEBUG: '\x1b[36m', INFO: '\x1b[32m' };
-		let desc = gLevels[sym];
-	    if (desc.importance >= gLevel.importance) {
-	        const dateStr = date(gDateFormat);
-	        let line = (typeof msg === 'string')? msg : hsNode.inspect(msg, null, gColors);
-	        line = gColors? ((color[sym]||"") + dateStr + ' ' + gPrefix + desc.desc + '\x1b[0m ' + line) :
-                            (dateStr + ' ' + gPrefix + desc.desc + ' ' + line);
-	        console.log(line);
-	        if (msg.stack) { console.log(msg.stack); }
-	        if (gLogFile) {
-	        	const filename = date(gLogFile);
-	        	fsUtil.appendFile(filename, line+'\n').catch(e => { 
+        let desc = gLevels[sym];
+        if (desc.importance >= gLevel.importance) {
+            const dateStr = date(gDateFormat);
+            let line = (typeof msg === 'string')? msg : inspect(msg, {depth:null, colors:gColors});
+            line = gColors? ((color[sym]||"") + dateStr + ' ' + this.gPrefix + desc.desc + '\x1b[0m ' + line) :
+                            (dateStr + ' ' + this.gPrefix + desc.desc + ' ' + line);
+            console.log(line);
+            if (msg.stack) { console.log(msg.stack); }
+            if (gLogFile) {
+                const filename = date(gLogFile);
+                fsUtil.appendFile(filename, line+'\n').catch(e => { 
                     console.log(`error appending to file ${gLogFile}: ${e}`); 
                     throw new Error(e); });
-	        }
-	    }
-	}
+            }
+        }
+    }
 
-    function defaultConfig(cfg:{colors?:boolean, logFile?:string, dateFormat?:string, level?:symbol }) {
+    config(cfg:{colors?:boolean, logFile?:string, dateFormat?:string, level?:symbol }) {
         let colors = true;
         if (cfg.colors!==undefined)     { gColors = colors = cfg.colors; }     // true / false
-        if (cfg.logFile!==undefined)    { logFile(cfg.logFile||undefined); }   // {logFile:null} => logFile(undefined)
-        if (cfg.dateFormat!==undefined) { dateFormat(cfg.dateFormat); }        // e.g. '%YYYY%MM%DD %hh:%mm:%ss.%jjj'
-        if (cfg.level!==undefined)      { level(cfg.level); }                  // e.g. INFO
-     }
-
-	return {
-		DEBUG:	    DEBUG,
-		INFO:	    INFO,
-		WARN:	    WARN,
-		ERROR:	    ERROR,
-		level:	    level,
-		debug:	    debug,
-		info: 	    info,
-		warn:	    warn,
-		error:	    error,
-		dateFormat: dateFormat,
-		prefix:	    prefix,
-		logFile:    logFile,
-        config:     defaultConfig
-	};
+        if (cfg.logFile!==undefined)    { this.logFile(cfg.logFile||undefined); }   // {logFile:null} => logFile(undefined)
+        if (cfg.dateFormat!==undefined) { this.dateFormat(cfg.dateFormat); }        // e.g. '%YYYY%MM%DD %hh:%mm:%ss.%jjj'
+        if (cfg.level!==undefined)      { this.level(cfg.level); }                  // e.g. INFO
+    }
 }
     
-export default log();
+export let log = new Log();
 
 
--- src/overview.ts
+++ src/overview.ts
@@ -4,6 +4,14 @@
 Helpful Scripts functions that wrap some nodejs calls in Promise wrappers.
 [Github page](https://github.com/HelpfulScripts/hsNode)
 
+ * ## Index of nodejs wrappers
+ * - &nbsp; {@link cpUtil child_processs} child process execution
+ * - &nbsp; {@link fsUtil file system} functions
+ * - &nbsp; {@link httpUtil http} web server functions
+
+ * ## Index of other node support functions
+ * - &nbsp; {@link log log} logging support
+ * - &nbsp; {@link excel excel} reading and writing Excel files
 
 
 */
