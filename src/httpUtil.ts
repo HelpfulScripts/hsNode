93eebc176d07d76a39424b8dbbd5f1f5b673a751
hs <>
2018-07-13 14:25:33 -0700

    fixed promises

--- src/httpUtil.ts
+++ src/httpUtil.ts
@@ -1,25 +1,22 @@
-const http    = require('http');
-
 /**
- * @ngdoc object
- * @name hsNode.httpUtil
- * @description Convenience functions for http access, wrapped in Promises.
+ * Convenience functions for http access, wrapped in Promises.
  * - {@link hsNode.httpUtil#methods_request request}
  */
+
+/** */
+const http  = require('http');
+
 
 //===============================================================================
 //  Low level Promise wrappers
  
 /**
- * @ngdoc object
- * @name request
- * @methodOf hsNode.httpUtil
- * @description sends a http request and promises to return the result.
- * @param {object} options the options to pass along to the request
- * @param {object} postData optional data to post
- * @return {Promise} promise to provide the result of the request.
+ * sends a http request and promises to return the result.
+ * @param options the options to pass along to the request
+ * @param postData optional data to post
+ * @return promise to provide the result of the request.
  */
-export function request(options:any, postData?:any) {
+export function request(options:any, postData?:any):Promise<any> {
     return new Promise((resolve:(out:string)=>void, reject:(e:any)=>void) => {
         let data = ''; 
         const req = http.request(options, (res:any) => {
@@ -36,17 +33,14 @@ export function request(options:any, postData?:any) {
 }
 
 /**
- * @ngdoc object
- * @name get
- * @methodOf hsNode.httpUtil
- * @description sends a http get request and promises to return the result.
+ * sends a http get request and promises to return the result.
  * @param {object} options the options to pass along to the get request
  * @return {Promise} promise to provide the result of the request.
  */
-export function get(options:any) {
+export function get(url:string):Promise<any> {
     return new Promise((resolve:(out:string)=>void, reject:(e:string)=>void) => {
         let data = ''; 
-        const req = http.get(options, (res:any) => {
+        const req = http.get(url, (res:any) => {
             res.setEncoding('utf8');
             res.on('data', (chunk:string) => { data += chunk; });
             res.on('end', () => resolve(data));
