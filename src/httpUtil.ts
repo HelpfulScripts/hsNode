2a44fcffc1de1ad3cdeb2b78d2e18bc3126086df
hs <>
2018-07-14 11:47:09 -0700

    started digest auth

--- src/httpUtil.ts
+++ src/httpUtil.ts
@@ -5,33 +5,7 @@
 
 /** */
 const http  = require('http');
-
-
-//===============================================================================
-//  Low level Promise wrappers
- 
-/**
- * sends a http request and promises to return the result.
- * @param options the options to pass along to the request
- * @param postData optional data to post
- * @return promise to provide the result of the request.
- */
-export function request(options:any, postData?:any):Promise<any> {
-    return new Promise((resolve:(out:string)=>void, reject:(e:any)=>void) => {
-        let data = ''; 
-        const req = http.request(options, (res:any) => {
-            res.setEncoding('utf8');
-            res.on('data', (chunk:string) => { data += chunk; });
-            res.on('end', () => resolve(data));
-        });
-        req.on('error', (e:any) => reject(e));
-
-        // write data to request body
-        if (postData !== undefined) { req.write(postData); }
-        req.end();
-    });
-}
-
+import { log } from './log'; log.prefix('http');
 
 export interface IncomingMessage {
     headers:        any;
@@ -44,10 +18,47 @@ export interface IncomingMessage {
     url:            string;
 }
 export interface HttpResponse {
-    response?:  IncomingMessage;
-    data?:      string;
-    error?:     string;
+    response:  IncomingMessage;
+    data:      string;
 }
+
+
+//===============================================================================
+//  Low level Promise wrappers
+ 
+/**
+ * sends a http request and promises to return the result.
+ * @param options the options to pass along to the request
+ * @param postData optional data to post
+ * @return promise to provide the result of the request.
+ */
+export function request(options:any, postData?:any):Promise<HttpResponse|string> {
+    return new Promise((resolve:(out:HttpResponse)=>void, reject:(e:string)=>void) => {
+        let data = ''; 
+        const req = http.request(options, (res:any) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk:string) => { data += chunk; });
+            res.on('end', () => {
+                if (res.headers['www-authenticate']) {
+                    const authReq = res.headers['www-authenticate'].split(',');
+                    const digest = {};
+                    authReq.forEach((a:string) => { 
+                        const kvp = a.split('=');
+                        digest[kvp[0]] = kvp[1];
+                    });
+                    log.info(`auth request with realm:'${digest['Digest realm']}', nonce:${digest['nonce']}, qop:${digest['qop']}`);
+                }
+                resolve({data:data, response:res});
+            });
+        });
+        req.on('error', (e:any) => reject(e));
+
+        // write data to request body
+        if (postData !== undefined) { req.write(postData); }
+        req.end();
+    });
+}
+
 
 /**
  * sends a http get request and promises to return the result.
@@ -55,14 +66,15 @@ export interface HttpResponse {
  * @return {Promise} promise to provide the result of the request.
  */
 export function get(url:string):Promise<HttpResponse|string> {
-    return new Promise((resolve:(out:HttpResponse)=>void, reject:(e:string)=>void) => {
-        let data = ''; 
-        const req = http.get(url, (res:any) => {
-            res.setEncoding('utf8');
-            res.on('data', (chunk:string) => { data += chunk; });
-            res.on('end', () => resolve({data:data, response:res}));
-        });
+    return request({method:'GET', uri:url});
+    // return new Promise((resolve:(out:HttpResponse)=>void, reject:(e:string)=>void) => {
+    //     let data = ''; 
+    //     const req = http.get(url, (res) => {
+    //         res.setEncoding('utf8');
+    //         res.on('data', (chunk:string) => { data += chunk; });
+    //         res.on('end', () => resolve({data:data, response:res}));
+    //     });
 
-        req.on('error', (e:string) => reject(e));
-    });
+    //     req.on('error', (e:string) => reject(e));
+    // });
 }
