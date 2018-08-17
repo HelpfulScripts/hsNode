49298009c7a0124dc590e907d761c843584e3448
hs <>
2018-08-17 09:23:20 -0700

    generalized cpUtil tests

--- src/cpUtil.jest.ts
+++ src/cpUtil.jest.ts
@@ -6,16 +6,16 @@ describe('cpUtil', () => {
         it('should execute "pwd" in a shell without error', () => 
             exec('pwd').then((result:{stdout:string, stderr:string}) => {
                 expect(typeof result.stdout).toBe('string');
-                expect(result.stdout.trim()).toMatch(/\/ts6\/dev\//); // macthes '.../ts6/dev/...' at the end of a string
+                expect(result.stdout.trim()).toMatch(/\/hsNode$/); // macthes '.../hsNode' at the end of a string
                 expect(result.stderr).toBe('');  
             })
         );
     });
 
     describe('invalid command', () => {
-        it('should execute "abcd" in a shell without error', () => 
+        it('should execute "abcd" in a shell *with* error', () => 
             exec('abcd').catch((err) => {
-                expect(err.toString().trim()).toMatch(/abcd: command not found/);
+                expect(err.toString().trim()).toMatch(/abcd: .*? not found/);
             })
         );
         it('should o x', () => {
