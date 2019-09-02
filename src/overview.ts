dcdb7f97ec9dff085302b04e41c92424c95d55dc
hs <helpfulscripts@gmail.com>
2019-09-02 16:50:17 -0700

    added badges to overview

--- src/overview.ts
+++ src/overview.ts
@@ -2,8 +2,17 @@
 # hsNode
 
 Helpful Scripts functions that wrap some nodejs calls in Promise wrappers.
-[`[Github page]`](https://github.com/HelpfulScripts/hsNode)
 [`[Coverage Info]`](./data/src/hsNode/coverage/)
+
+[![npm version](https://badge.fury.io/js/hsnode.svg)](https://badge.fury.io/js/hsnode)
+[![GitHub](https://img.shields.io/badge/GitHub-hsNode-blue.svg)](https://github.com/helpfulscripts/hsnode)
+[![docs](https://img.shields.io/badge/hsDocs-hsNode-blue.svg)](https://helpfulscripts.github.io/hsNode/#!/api/hsNode/0)
+[![Build Status](https://travis-ci.org/HelpfulScripts/hsNode.svg?branch=master)](https://travis-ci.org/HelpfulScripts/hsNode)
+[![Dependencies Status](https://david-dm.org/helpfulscripts/hsnode.svg)](https://david-dm.org/helpfulscripts/hsnode)
+[![Coverage Status](https://coveralls.io/repos/github/HelpfulScripts/hsNode/badge.svg?branch=master)](https://coveralls.io/github/HelpfulScripts/hsNode?branch=master)
+[![Known Vulnerabilities](https://snyk.io/test/github/HelpfulScripts/hsNode/badge.svg?targetFile=package.json)](https://snyk.io/test/github/HelpfulScripts/hsNode?targetFile=package.json)
+[![NPM License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://www.npmjs.com/package/hsnode)
+
 ___
 
 ## Index of nodejs wrappers
