0fc38d41e347c4ab1dd6d200485151a492a7554a
hs <helpfulscripts@gmail.com>
2020-11-07 13:49:22 -0800

    removed commenting

--- src/Request.ts
+++ src/Request.ts
@@ -148,18 +148,6 @@ export class Request extends RequestUtil {
 
         const response = await super.requestOptions(options, useCached, postData);
         
-        // let request: Promise<Response>;
-        // if (this.pace) {
-        //     this.log.transient(`(${this.pace.inQueue()} | ${this.pace.inProgress()}) ${options.method}-ing ${options.url}`); 
-        //     request = this.pace.add(() => this.request(options, postData), `${options.method} ${options.url}`);
-        // } else {
-        //     request = this.request(options, postData);
-        // }
-        // // this.log.debug(()=>`${options.method}-ing ${options.url}`); 
-        // const response = await request;
-        // if (this.pace) { this.log.transient(`(${this.pace.inQueue()} | ${this.pace.inProgress()}) received ${options.method} ${options.url} `); }
-        // // this.log.debug(()=>`received ${options.method} ${response.response.statusMessage} ${options.method} ${options.url}`); 
-
         const code = response.response.statusCode||response.response.status;
         if(code >= 200 && code < 300) {
             if (fname && options.method === 'GET') {
